3c0129203aa703fcd280e43bb1d8f7bdd4bbd336
JYEJYE <73331620+shubiduba1212@users.noreply.github.com>
2024-04-01 03:29:27 +0900

    Update Loading.js

--- humming_connect/forUploading/Loading.js
+++ humming_connect/forUploading/Loading.js
@@ -4,7 +4,11 @@ import loadingStyle from "./Loading.module.css";
 export default function LoadingSpinner() {
   return (
     <div className={loadingStyle.spinnerContainer}>
-      <div className={loadingStyle.loadingSpinner}>
+      <div className={loadingStyle.spinnerContents}>
+        <img src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="Humming Connect logo"/>
+        <p className={loadingStyle.loadingText}>Humming</p>
+        <p className={loadingStyle.loadingText}>Connect</p>
+        <div className={loadingStyle.loadingSpinner}></div>
       </div>
     </div>
   );
